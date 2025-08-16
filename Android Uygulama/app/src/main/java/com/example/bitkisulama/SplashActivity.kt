d43f8045d9c4a324cabde5c97bc9ef9dac39531f
nyaex <70057364+nyaexx@users.noreply.github.com>
2025-08-16 19:26:37 +0300

    Splash Screen geciktirmesi azaltıldı.

--- Android Uygulama/app/src/main/java/com/example/bitkisulama/SplashActivity.kt
+++ Android Uygulama/app/src/main/java/com/example/bitkisulama/SplashActivity.kt
@@ -15,6 +15,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1000)
+        }, 600)
     }
 }
